d5e2b81ff4f6f7482baf18e66aae11fef27d28ea
eq9684 <eq9684@hotmail.com>
2022-04-10 14:09:57 +0800

    Create cloudwatch-agent.sh

---
+++ cloudwatch-agent.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+sudo yum install amazon-cloudwatch-agent -y > /amazon-cloudwatch-agent.log
+cat >/opt/aws/amazon-cloudwatch-agent/bin/config.json<<EOF
+{
+	"agent": {
+		"metrics_collection_interval": 60,
+		"run_as_user": "root"
+	},
+	"metrics": {
+		"aggregation_dimensions": [
+			[
+				"InstanceId"
+			]
+		],
+		"append_dimensions": {
+			"AutoScalingGroupName": "\${aws:AutoScalingGroupName}",
+			"ImageId": "\${aws:ImageId}",
+			"InstanceId": "\${aws:InstanceId}",
+			"InstanceType": "\${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"disk": {
+				"measurement": [
+					"used_percent"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 60
+			},
+			"statsd": {
+				"metrics_aggregation_interval": 60,
+				"metrics_collection_interval": 10,
+				"service_address": ":8125"
+			}
+		}
+	}
+}
+EOF
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
